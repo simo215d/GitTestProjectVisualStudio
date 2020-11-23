7cb11c59547b335dd1e38f149bf2aafe4c870ece
Simon Grove <simo215d@edu.easj.dk>
2020-11-23 20:22:52 +0100

    Add project files.

---
+++ GitTestProjectVisualStudio/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitTestProjectVisualStudio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
