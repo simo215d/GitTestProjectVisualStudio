5f62647114831dc1377276e2cb8a859087d737c2
Simon Grove <simo215d@edu.easj.dk>
2020-11-25 18:18:24 +0100

    first commit from VS EVUR !!!1

--- GitTestProjectVisualStudio/Program.cs
+++ GitTestProjectVisualStudio/Program.cs
@@ -6,7 +6,7 @@ namespace GitTestProjectVisualStudio
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World! Damn son!");
         }
     }
 }
